07e4911754f16c8c6de24e3ca4256ff2f311683e
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-29 16:35:16 +0530

    Create sample.s
    This is code after compiling phase.
    This is assembly language specific to my hardware i.e. Assembler

---
+++ Phases/sample.s
@@ -0,0 +1,60 @@
+	.file	"sample.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Enter your age: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Yes, You can vote."
+	.align 8
+.LC3:
+	.string	"Opps, You are not allowed to vote."
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -12(%rbp)
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-12(%rbp), %eax
+	cmpl	$17, %eax
+	jle	.L2
+	leaq	.LC2(%rip), %rdi
+	call	puts@PLT
+	jmp	.L3
+.L2:
+	leaq	.LC3(%rip), %rdi
+	call	puts@PLT
+.L3:
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 8.3.0-6ubuntu1) 8.3.0"
+	.section	.note.GNU-stack,"",@progbits
