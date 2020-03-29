2f51ce2f0c230b5b17a065d603a8c64e168c13ba
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-30 02:43:22 +0530

    Create isPrime.c

---
+++ BasicPrograms/isPrime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int num = 0;
+
+	printf("Enter number to check prime or not: ");
+	scanf("%d", &num);
+	
+	int isPrime = 1;
+
+	for(int i = 2; i*i <= num; i++)
+	{
+		if(num % i == 0)
+		{
+			isPrime = 0;
+			break;
+		}
+	}
+	
+	if(isPrime)
+		printf("%d is Prime!!!\n", num);
+	else
+		printf("%d is not Prime!!!\n", num);
+	
+	return 0;
+}
