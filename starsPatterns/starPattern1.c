b7cf793773f37c8d1c76c1fca7a4d83a54f7afd5
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-30 14:46:53 +0530

    Create starPattern1.c

---
+++ starsPatterns/starPattern1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int numberOfRows = 0;
+
+	printf("Enter number of rows: ");
+	scanf("%d", &numberOfRows);
+	
+	for(int i = 1; i <= numberOfRows; i++)
+	{
+		for(int j = 1; j <= numberOfRows - i; j++)
+		{
+			printf(" ");
+		}	
+		for(int k = 1; k <= 2*i - 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
